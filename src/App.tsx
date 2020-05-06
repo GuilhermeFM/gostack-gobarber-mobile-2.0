4ae7fa030882531ee9e99859c2b0607cf99a6aea
Guilherme F Meira <guifrizzera2005@gmail.com>
2020-05-06 12:25:35 -0300

    Installing react-navigation and adding basic stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor="#312E38" />
+      <Routes />
+    </NavigationContainer>
+  );
 };
 
 export default App;
